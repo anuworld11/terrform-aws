c9ad637ef33cc850ca56b6892f1c83285de00048
anuworld11 <anuworld11@gmail.com>
2025-05-19 02:24:05 +0530

    till random variables

--- main.tf
+++ main.tf
@@ -1,22 +1,29 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.0.0-beta1"
-    }
-  }
-}
 
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
-}
-/*resource "aws_instance" "myfirstinstance" {
-  ami           = "ami-0953476d60561c955" # Amazon Linux 2 AMI
+resource "aws_instance" "myfirstinstance" {
+  ami           = var.ami # Amazon Linux 2 AMI
   instance_type = "t2.nano"
+tags = {
+    Name = "MyFirstInstance"
+}
+}
+resource "random_id" "rand" {
+  byte_length = 8
+}
 
+resource "aws_s3_bucket" "mytetsbucket" {
+  bucket = "my-test-${random_id.rand.hex}" # Replace with a unique bucket name
+  region = "eu-west-1"
 
   tags = {
-    Name = "MyFirstInstance"
+    Name = "MyTestBucket"
   }
+
+}
+/*resource "aws_s3_object" "mydata" {
+  bucket = aws_s3_bucket.mytetsbucket.id
+  key    = "data.txt"
+  source = "mydata.txt"
+  region = "eu-west-1"
+
 }*/
+
